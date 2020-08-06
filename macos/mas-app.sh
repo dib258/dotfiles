dc8db8c3e68d3767265e34b09f1d0918ae9c4a95
dib258 <lesmanne@gmail.com>
2020-08-06 23:29:14 +0200

    Extracted other list of software to external files. WIP to ask if it needs to be installed.

---
+++ macos/mas-app.sh
@@ -0,0 +1,36 @@
+echo 'Install mas (Mac Apple Store)'
+echo '-----------------------------'
+brew install mas-cli/tap/mas
+
+echo 'Install Xcode'
+echo '-------------'
+mas install 497799835
+xcode-select --install
+
+echo 'Install Bear'
+echo '------------'
+mas install 1091189122
+
+echo 'Install Bitwarden'
+echo '------------'
+mas install 1352778147
+
+echo 'Install Sequel Ace'
+echo '------------------'
+mas install 1518036000
+
+echo 'Install Slack'
+echo '--------------'
+mas install 803453959
+
+echo 'Install Spark'
+echo '--------------'
+mas install 1176895641
+
+echo 'Install Termius'
+echo '---------------'
+mas install 1176074088
+
+echo 'Install uBlock'
+echo '---------------'
+mas install 1385985095
---
+++ macos/work-app.sh
@@ -0,0 +1,67 @@
+echo 'Install Usefull Tools'
+echo '--------------'
+brew install curl htop nmap links geoip watch ffmpeg speedtest-cli tldr jq taskwarrior wifi-password youtube-dl gnupg libpng jpeg highlight ssh-copy-id
+
+echo 'Install Alfred'
+echo '--------------'
+brew cask install alfred
+
+echo 'Install Visual Studio Code'
+echo '--------------------------'
+brew cask install visual-studio-code
+
+echo 'Install VirtualBox'
+echo '--------------'
+brew cask install virtualbox
+
+echo 'Install vagrant'
+echo '--------------'
+brew cask install vagrant
+
+echo 'Install Tinkerwell'
+echo '------------------'
+brew cask install tinkerwell
+
+echo 'Install Spectacle'
+echo '-----------------'
+brew cask install spectacle
+
+echo 'Install Skype'
+echo '-------------'
+brew cask install skype
+
+echo 'Install Postman'
+echo '---------------'
+brew cask install postman
+
+echo 'Install Opera'
+echo '-------------'
+brew cask install opera
+
+echo 'Install Firefox'
+echo '--------------'
+brew cask install firefox
+
+echo 'Install Google Chrome'
+echo '--------------'
+brew cask install google-chrome 
+
+echo 'Install Dropbox'
+echo '--------------'
+brew cask install dropbox
+
+echo 'Install Keka'
+echo '--------------'
+brew cask install keka
+
+echo 'Install Notion'
+echo '--------------'
+brew cask install notion
+
+echo 'Install Forticlient'
+echo '--------------'
+brew cask install forticlient
+
+echo 'Install GrandPerspective'
+echo '--------------'
+brew cask install grandperspective
