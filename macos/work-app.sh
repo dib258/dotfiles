694b97c97e6b85f58d3c0c96c1bda3afe30f719c
Ludo <dib258@Ludos-MacBook-Pro.local>
2020-08-13 16:02:33 +0200

    Corrected some errors (composer, condition before rm), Prettified design.

--- macos/work-app.sh
+++ macos/work-app.sh
@@ -1,63 +1,79 @@
+echo '---------------------'
 echo 'Install Usefull Tools'
-echo '--------------'
+echo '---------------------'
 brew install curl htop nmap links geoip watch ffmpeg speedtest-cli tldr jq wifi-password youtube-dl gnupg libpng jpeg highlight ssh-copy-id
 
+echo '--------------------------'
 echo 'Install Visual Studio Code'
 echo '--------------------------'
 brew cask install visual-studio-code
 
+echo '-------------'
 echo 'Install Slack'
 echo '-------------'
 brew cask install slack
 
+echo '------------------'
 echo 'Install VirtualBox'
-echo '--------------'
-brew cask install virtualbox
+echo '------------------'
+brew cask install --force virtualbox
 
+echo '---------------'
 echo 'Install vagrant'
-echo '--------------'
+echo '---------------'
 brew cask install vagrant
 
+echo '------------------'
 echo 'Install Tinkerwell'
 echo '------------------'
 brew cask install tinkerwell
 
+echo '-----------------'
 echo 'Install Spectacle'
 echo '-----------------'
 brew cask install spectacle
 
+echo '---------------'
 echo 'Install Postman'
 echo '---------------'
 brew cask install postman
 
+echo '---------------------'
 echo 'Install Google Chrome'
-echo '--------------'
+echo '---------------------'
 brew cask install google-chrome 
 
+echo '------------------'
 echo 'Install Sequel Ace'
 echo '------------------'
 brew cask install sequel-ace
 
+echo '---------------'
 echo 'Install Dropbox'
-echo '--------------'
+echo '---------------'
 brew cask install dropbox
 
+echo '------------'
 echo 'Install Keka'
-echo '--------------'
+echo '------------'
 brew cask install keka
 
+echo '--------------'
 echo 'Install Notion'
 echo '--------------'
 brew cask install notion
 
+echo '-----------------'
 echo 'Install Bitwarden'
 echo '-----------------'
 brew cask install bitwarden
 
+echo '-------------'
 echo 'Install F.lux'
 echo '-------------'
 brew cask install flux
 
+echo '---------------'
 echo 'Install Spotify'
 echo '---------------'
 brew cask install spotify
